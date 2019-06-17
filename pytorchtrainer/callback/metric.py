08cd490310e99ee69cb83ec852020a9011d47e42
Benoit Martin <benoitmartin88.pro@gmail.com>
2019-06-17 15:07:53 +0200

    fix accuracy bug

--- pytorchtrainer/callback/metric.py
+++ pytorchtrainer/callback/metric.py
@@ -5,8 +5,14 @@ class MetricCallback(Callback):
     def __init__(self, metric, frequency=10):
         super().__init__(frequency=frequency, state_attribute_name=metric.name)
         self.metric = metric
+        self.last_epoch = 0
 
     def __call__(self, trainer):
+        if self.last_epoch != trainer.state.current_epoch:
+            self.metric.reset()
+
+        self.last_epoch = trainer.state.current_epoch
+
         self.metric.step(trainer.state.last_y, trainer.state.last_y_pred)
         setattr(trainer.state, self.state_attribute_name, self.metric.compute())
 

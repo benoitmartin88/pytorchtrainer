5ca8274a79747da621330077c5777d82a44055ae
Benoit Martin <benoitmartin88.pro@gmail.com>
2019-10-02 17:28:12 +0200

    new try

--- setup_pyenvs.sh
+++ setup_pyenvs.sh
@@ -14,5 +14,5 @@ PYTHON_VERSIONS=(3.5.7 3.6.9 3.7.4)
 for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
     echo "Setting pyenv python version ${PYTHON_VERSION}";
     pyenv install -s ${PYTHON_VERSION}
-    PATH=${PYENV_VERSION_DIR}/${PYTHON_VERSION}/bin:${PATH}
+    export PATH=${PYENV_VERSION_DIR}/${PYTHON_VERSION}/bin:${PATH}
 done
